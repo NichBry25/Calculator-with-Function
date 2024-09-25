f15af529c1250994634c90cb2c43e98622b9ba62
NichBry25 <nicholas.bryan001@binus.ac.id>
2024-09-25 21:55:59 +0700

    Update funccalc.py

--- funccalc.py
+++ funccalc.py
@@ -1,20 +1,3 @@
-# def make_shirt(size, message):
-#     print(f"I made a shirt!")
-#     print(f"The size of my shirt is {size}, with a message on it which is {message}.")
-# make_shirt(message="Dog", size="20")
-
-# messages = ["Gyattzers", "Skibidi"]
-# def show_message(message):
-#     for sentence in message:
-#         print(sentence)
-# show_message(messages)
-
-# import math
-# from math import sqrt
-# from math import sqrt as root
-# from math import *
-# print(root(64))
-
 number1 = int(input("Enter a number: "))
 number2 = int(input("Enter another number: "))
 op = input("Enter an operator (+, -, *, /, **): ")
